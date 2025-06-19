ffb690409d02ef9767f7f9244bbf3fdd5894bffa
AngeloNorelli <angnorelli@gmail.com>
2025-06-19 12:17:14 +0200

    dodanie możliwość logowania

--- fullstack-app/backend/src/controllers/authController.ts
+++ fullstack-app/backend/src/controllers/authController.ts
@@ -1,30 +1,65 @@
 import { Request, Response } from "express";
 import { User } from "../model/User";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
 export const register = async (req: Request, res: Response) => {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    try {
-        // Sprawdź, czy użytkownik już istnieje
-        const existingUser = await User.findOne({ where: { email } });
-        if (existingUser) {
-            return res.status(400).json({ message: "User with this email already exists." });
-        }
-
-        // Hashowanie hasła
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Tworzenie nowego użytkownika
-        const newUser = await User.create({
-            username,
-            email,
-            password: hashedPassword,
-        });
-
-        res.status(201).json({ message: "User registered successfully.", user: newUser });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "An error occurred during registration." });
+  try {
+    // Sprawdź, czy użytkownik już istnieje
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+        return res.status(400).json({ message: "User with this email already exists." });
     }
+
+    // Hashowanie hasła
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Tworzenie nowego użytkownika
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: "User registered successfully.", user: newUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred during registration." });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required." });
+  }
+
+  try {
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password." });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid email or password." });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, username: user.username, email: user.email }, 
+      JWT_SECRET, 
+      { expiresIn: "6h" }
+    );
+
+    res.status(200).json({ message: "Login successful.", token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred during login." });
+  }
 };
